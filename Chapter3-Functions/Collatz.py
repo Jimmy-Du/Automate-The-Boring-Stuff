c66bf5d2462d32b4b2054ce76593d54d7d660d38
Jimmy-Du <jdu5780@conestogac.on.ca>
2020-04-26 18:15:27 -0400

    Added Comments.

--- Chapter3-Functions/Collatz.py
+++ Chapter3-Functions/Collatz.py
@@ -1,16 +1,33 @@
+# File:        Collatz.py
+# Date:        April 26, 2020
+# Description: Asks user for an integer input and performs calculations on that number until the result is 1.
+
+
+
+# Function:    collatz()
+# Description: takes in a number divides it by 2 if the number is even, if the number is odd, it is multiplied by 3
+#              and incremented by 1. The result of the calculation is returned.
+# Parameters:  number: the number that the calculations will use
+# Return:      the resulting number from the calculation
 def collatz(number):
+    # if the number is even, the number will be divided in half
     if number % 2 == 0 :
         return number / 2
+    # else, the number is odd and will be multiplied by 3 and incremented by 1
     else:
         return 3 * number + 1
 
 
+
+# try block that asks for an whole number input from the user
 try:
     userNum = int(input('Please enter an integer value:\n'))
     
+    # loop that performs the collatz calculation and prints the result until it results in 1
     while userNum != 1:
         userNum = int(collatz(userNum))
         print(userNum)
 
+# if the user does not enter a whole number, an error message is displayed
 except ValueError:
     print('Input was not an integer')
--- Chapter4-Lists/CoinFlipStreaks.py
+++ Chapter4-Lists/CoinFlipStreaks.py
@@ -1,23 +1,38 @@
+# File:        CoinFlipStreaks.py
+# Date:        April 26, 2020
+# Description: Generates 10000 coin flip results and keeps track of how many times a head or tail result appears
+#              6 times in a row 
+
+
+
 import random
 
+
+
+# counters to hold the number of streaks, number of head and tail coin flips in a row
 numberOfStreaks = 0
 numOfHeadsInRow = 0
 numOfTailsInRow = 0
 
+# list to hold the coin flip results
 coinFlips = []
 
+# creates a list of 10000 random 'heads' or 'tails' values.
 for experimentNumber in range(10000):
-    # creates a list of 100 random 'heads' or 'tails' values.
+    # if the randomly generated number is 0 indicating heads, an 'H' is appended to the list and
+    # the counter of the heads in a row is incremented and the counter of tails in a row is reset to 0
     if random.randint(0,1) == 0:
         coinFlips.append('H')
         numOfHeadsInRow += 1
         numOfTailsInRow = 0
+    # else, an 'T' is appended to the list and the counter of the tails in a row is incremented and the 
+    # counter of heads in a row is reset to 0
     else:
         coinFlips.append('T')
         numOfTailsInRow += 1
         numOfHeadsInRow = 0
 
-    # checks if there is a streak of 6 heads or tails in a row.
+    # checks if there is a streak of 6 heads or tails in a row, and will increment the streak counter.
     if numOfTailsInRow > 1 and numOfTailsInRow % 6 == 0 or numOfHeadsInRow % 6 == 0 and numOfHeadsInRow > 1:
         numberOfStreaks += 1
 
